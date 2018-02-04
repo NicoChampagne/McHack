5abdb129ebbc50acd6cafbf78f2b36d357c75fa8
AjmerSingh7537 <a_gadreh@encs.concordia.ca>
2018-02-04 11:58:54 -0500

    first picture

--- backend/imageProcessingService.js
+++ backend/imageProcessingService.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const vision = require('@google-cloud/vision');
 const client = new vision.ImageAnnotatorClient();
-// const Canvas = require('canvas');
+const Canvas = require('canvas');
 const fs = require('fs');
 
 
@@ -65,54 +65,6 @@ function detectFaces(img) {
     return dataResults;
   }
 
-<<<<<<< HEAD
-  // function addSunGlassesToFaces(faces, inputImage, outputImage, Canvas, callback) {
-  //   fs.readFile(inputImage, (err, image) => {
-  //   if (err) {
-  //     return callback(err);
-  //   }
-  //
-  //   var Image = Canvas.Image;
-  //   // Open the original image into a canvas
-  //   var img = new Image();
-  //   img.src = image;
-  //   var canvas = new Canvas(img.width, img.height);
-  //   var context = canvas.getContext('2d');
-  //   context.drawImage(img, 0, 0, img.width, img.height);
-  //
-  //   Now draw boxes around all the faces
-  //   context.drawImage('./res/test-images/sunglasses.png', 33, 71, 104, 124, 21, 20, 87, 104);
-  //   context.strokeStyle = 'rgba(0,255,0,0.8)';
-  //   context.lineWidth = '5';
-  //
-  //   faces.forEach(face => {
-  //     context.beginPath();
-  //     let origX = 0;
-  //     let origY = 0;
-  //     face.boundingPoly.vertices.forEach((bounds, i) => {
-  //       if (i === 0) {
-  //         origX = bounds.x;
-  //         origY = bounds.y;
-  //       }
-  //       context.lineTo(bounds.x, bounds.y);
-  //     });
-  //     context.lineTo(origX, origY);
-  //     context.stroke();
-  //   });
-  //
-  //   // Write the result to a file
-  //   console.log('Writing to file ' + outputImage);
-  //   var writeStream = fs.createWriteStream(outputImage);
-  //   var pngStream = canvas.pngStream();
-  //
-  //   pngStream.on('data', chunk => {
-  //     writeStream.write(chunk);
-  //   });
-  //   pngStream.on('error', console.log);
-  //   pngStream.on('end', callback);
-  // });
-  // }
-=======
   function addSunGlassesToFaces(faces, inputImage, outputImage, Canvas, callback) {
     fs.readFile(inputImage, (err, image) => {
     if (err) {
@@ -158,7 +110,6 @@ function detectFaces(img) {
     pngStream.on('end', callback);
   });
   }
->>>>>>> c01a7f3553ff290f00499c0ea40ec903f59ef6e7
 
  module.exports = {
   detectImageLabels: detectImageLabels,
