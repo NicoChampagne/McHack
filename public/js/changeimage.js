9986830e9b3f4c8cdd12e1fe5d7f835c64667903
niconoloco <nicolas.b.champagne@gmail.com>
2018-02-04 17:34:02 -0500

    fixed changing images, only hover problem is still there

--- public/js/changeimage.js
+++ public/js/changeimage.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-function changeImage(ele) {
 
-        document.getElementById('featuredImg').src = "//"+ele;
-        return document.getElementById('featuredImg').src;
-=======
+
 function changeImage(ele,newclassname) {
         document.getElementById('featuredImg').src = ele;
         document.getElementById('featuredImg').className =newclassname;
@@ -27,5 +23,5 @@ function prevImage() {
      var b =document.getElementById(a).src;
 
      changeImage(b,a);
->>>>>>> 1d166ac93dd8a58912382109e3d582361b1d021d
+
 }
